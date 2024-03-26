d460a003039a166e20a793f057e7ba8f9b265368
Amit Mohapatra <mohapatra.amit2002@gmail.com>
2024-03-26 11:51:07 +0530

    Update Train.java

--- java-files/Train.java
+++ java-files/Train.java
@@ -1,3 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Train {
+    private String trainNumber;
+    private TrainRoute route;
+    private Map<CoachType, Integer> seatsAvailable
+    private Map<CoachType, Double> fare;
+    
+    public Train(String trainNumber, TrainRoute route) {
+        this.trainNumber = trainNumber;
+        this.route = route;
+        this.seatsAvailable = new HashMap<>();
+        this.fare = new HashMap<>();
+    }
+
+    boolean checkAvailability(CoachType coachType) {
+        if (seatsAvailable.containsKey(coachType)) {
+            return seatsAvailable.get(coachType) > 0;
+        } else {
+            return false;
+        }
+    }
 
 }
