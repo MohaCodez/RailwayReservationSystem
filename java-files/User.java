7d7a9d50f41ef9333ffe742cfc3d0c1ffcbf0f80
Deven <f20212821@goa.bits-pilani.ac.in>
2024-03-26 10:41:33 +0530

    Update User.java
    wrote basic code

--- java-files/User.java
+++ java-files/User.java
@@ -1,2 +1,34 @@
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    public boolean isUserLoggedIn;
+
+    // constructor
+    User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.isUserLoggedIn = false;
+    }
+
+    // getters and setters for the user's attributes
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    // other methods
+    public boolean login() {
+        this.isUserLoggedIn = true;
+        return true;
+    }
+
+    public void logout() {
+        this.isUserLoggedIn = false;
+    }
+
 }
