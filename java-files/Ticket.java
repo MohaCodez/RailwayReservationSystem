c85177596393fca608cfd43e1e930fe14c0c0343
Deven <f20212821@goa.bits-pilani.ac.in>
2024-03-26 10:58:10 +0530

    Update Ticket.java
    created class

--- java-files/Ticket.java
+++ java-files/Ticket.java
@@ -1,3 +1,20 @@
 public class Ticket {
-    
+    private String ticketId;
+    private Passenger passenger;
+    private Train train;
+    public double fare;
+    private BookingStatus status;
+
+    Ticket() {
+        // default constructor. To be filled in later
+    }
+
+    Ticket(String ticketId, Passenger passenger, Train train, double fare, BookingStatus status) {
+        this.ticketId = ticketId;
+        this.passenger = passenger;
+        this.train = train;
+        this.fare = fare;
+        this.status = status;
+        this.status = status;
+    }
 }
